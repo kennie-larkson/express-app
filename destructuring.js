d884e704cb6854cd9ba5dcb98546e811f4a58203
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-05 10:14:03 +0100

    added new destructuring file

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,19 @@
-const names = [
-    { name: 'Kehinde'},{ name: 'Taiwo'},{ name: 'Tope'},{ name: 'Wale'},
-    { name: 'Segun'},{ name: 'Yinka'},{ name: 'Bolanle'},{ name: 'Jadesola'}
-            ];
+const alphabets = ['A','B','C','D','E','F'];
+const numbers = ['1','2','3','4','5','6'];
+
+//  const a = alphabets[0];
+//  const b = alphabets[1];
+
+const [ a, , c, ...rest] = alphabets
+//  console.log(a);
+//  console.log(c);
+//  console.log(rest);
+
+
+const newArray = [...alphabets, ...numbers];
+
+ console.log(newArray);
+
+
+
+
