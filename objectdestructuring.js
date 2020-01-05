e310443d66220bc8feff89a58c345294a6970e2b
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-01-05 11:22:56 +0100

    first commit

---
+++ objectdestructuring.js
@@ -0,0 +1,8 @@
+const personOne = {
+    name: 'Kennie',
+    age: 33,
+    canEat: ()=> console.log(this.name+' can eat'),
+    canCode: ()=> console.log(this.name+' can code'),
+    canSing: ()=> console.log(this.name+' can sing')
+
+}
